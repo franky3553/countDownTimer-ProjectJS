94cf1876ff21c5474629bd5f9ee01cd587744617
franky3553 <franky3553@gmail.com>
2022-07-07 09:43:05 +0200

    ggupdt

--- app.js
+++ app.js
@@ -21,3 +21,33 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 4, 24, 17, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const seconds = futureDate.getSeconds();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekDays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTIme(){
+  const today = new Date().getTime();
+  const t = futureTime -today;
+
+}
+
+getRenmainingTime();
